84b8932abddc6bd1417ce9a67a5deee7c194f902
gkapitein <84490555+gkapitein@users.noreply.github.com>
2021-06-09 19:38:11 +0200

    Add files via upload

---
+++ javascript odracht else if/script.js
@@ -0,0 +1,21 @@
+let myAge = 14;
+let isFemale = false;
+let driverStatusBOB = true;
+
+if (myAge > 18) {
+    console.log("your are older then 18");
+} else if (myAge < 18) {
+    console.log("Youre not old enough please stay outside");
+}
+
+if (isFemale == true) {
+    console.log("this visitor is a female");
+} else if (isFemale == false) {
+    console.log("this visitor is not a female");
+}
+
+if (driverStatusBOB = true) {
+    console.log("driver is allowed to drive");
+} else {
+    console.log("driver is not allowed to drive");
+}
